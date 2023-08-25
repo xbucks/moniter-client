e4c692a49cda7a65ea26e1230c9fd148ab506ed7
xbucks <jmy.kot@gmail.com>
2023-08-26 09:57:24 +1000

    fix capture func

--- src/capture.rs
+++ src/capture.rs
@@ -1,5 +1,9 @@
 use anyhow::{anyhow, Result};
 use image::RgbaImage;
+use crate::capture_screen;
+use crate::capture_screen_area;
+
+pub use display_info::DisplayInfo;
 
 pub fn vec_to_rgba_image(width: u32, height: u32, buf: Vec<u8>) -> Result<RgbaImage> {
     RgbaImage::from_vec(width, height, buf).ok_or(anyhow!("buffer not big enough"))
@@ -15,6 +19,81 @@ pub fn bgra_to_rgba_image(width: u32, height: u32, buf: Vec<u8>) -> Result<RgbaI
         .collect();
 
     vec_to_rgba_image(width, height, rgba_buf)
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Screen {
+    pub display_info: DisplayInfo,
+}
+
+impl Screen {
+    pub fn new(display_info: &DisplayInfo) -> Self {
+        Screen {
+            display_info: *display_info,
+        }
+    }
+
+    pub fn all() -> Result<Vec<Screen>> {
+        let screens = DisplayInfo::all()?.iter().map(Screen::new).collect();
+        Ok(screens)
+    }
+
+    pub fn from_point(x: i32, y: i32) -> Result<Screen> {
+        let display_info = DisplayInfo::from_point(x, y)?;
+        Ok(Screen::new(&display_info))
+    }
+
+    pub fn capture(&self) -> Result<RgbaImage> {
+        capture_screen(&self.display_info)
+    }
+
+    /**
+     * 截取指定区域
+     * 区域x,y为相对于当前屏幕的x,y坐标
+     */
+    pub fn capture_area(&self, x: i32, y: i32, width: u32, height: u32) -> Result<RgbaImage> {
+        let display_info = self.display_info;
+        let screen_x2 = display_info.x + display_info.width as i32;
+        let screen_y2 = display_info.y + display_info.height as i32;
+
+        let mut x1 = x + display_info.x;
+        let mut y1 = y + display_info.y;
+        let mut x2 = x1 + width as i32;
+        let mut y2 = y1 + height as i32;
+
+        // x y 必须在屏幕范围内
+        if x1 < display_info.x {
+            x1 = display_info.x;
+        } else if x1 > screen_x2 {
+            x1 = screen_x2
+        }
+
+        if y1 < display_info.y {
+            y1 = display_info.y;
+        } else if y1 > screen_y2 {
+            y1 = screen_y2;
+        }
+
+        if x2 > screen_x2 {
+            x2 = screen_x2;
+        }
+
+        if y2 > screen_y2 {
+            y2 = screen_y2;
+        }
+
+        if x1 >= x2 || y1 >= y2 {
+            return Err(anyhow!("Area size is invalid"));
+        }
+
+        capture_screen_area(
+            &display_info,
+            x1 - display_info.x,
+            y1 - display_info.y,
+            (x2 - x1) as u32,
+            (y2 - y1) as u32,
+        )
+    }
 }
 
 /// Some platforms e.g. MacOS can have extra bytes at the end of each row.
--- src/lib.rs
+++ src/lib.rs
@@ -29,7 +29,7 @@ pub use crate::zip::read::ZipArchive;
 pub use crate::zip::types::DateTime;
 pub use crate::zip::write::ZipWriter;
 pub use crate::zip::result::ZipResult;
-pub use display_info::DisplayInfo;
+pub use crate::capture::Screen;
 pub use zip::write::FileOptions;
 
 use anyhow::{anyhow, Result};
@@ -183,81 +183,5 @@ mod tests {
         assert_eq!(char_c, "c".to_string());
         let n = keyboard.add(&EventType::KeyRelease(Key::KeyS));
         assert_eq!(n, None);
-    }
-}
-
-
-#[derive(Debug, Clone, Copy)]
-pub struct Screen {
-    pub display_info: DisplayInfo,
-}
-
-impl Screen {
-    pub fn new(display_info: &DisplayInfo) -> Self {
-        Screen {
-            display_info: *display_info,
-        }
-    }
-
-    pub fn all() -> Result<Vec<Screen>> {
-        let screens = DisplayInfo::all()?.iter().map(Screen::new).collect();
-        Ok(screens)
-    }
-
-    pub fn from_point(x: i32, y: i32) -> Result<Screen> {
-        let display_info = DisplayInfo::from_point(x, y)?;
-        Ok(Screen::new(&display_info))
-    }
-
-    pub fn capture(&self) -> Result<RgbaImage> {
-        capture_screen(&self.display_info)
-    }
-
-    /**
-     * 截取指定区域
-     * 区域x,y为相对于当前屏幕的x,y坐标
-     */
-    pub fn capture_area(&self, x: i32, y: i32, width: u32, height: u32) -> Result<RgbaImage> {
-        let display_info = self.display_info;
-        let screen_x2 = display_info.x + display_info.width as i32;
-        let screen_y2 = display_info.y + display_info.height as i32;
-
-        let mut x1 = x + display_info.x;
-        let mut y1 = y + display_info.y;
-        let mut x2 = x1 + width as i32;
-        let mut y2 = y1 + height as i32;
-
-        // x y 必须在屏幕范围内
-        if x1 < display_info.x {
-            x1 = display_info.x;
-        } else if x1 > screen_x2 {
-            x1 = screen_x2
-        }
-
-        if y1 < display_info.y {
-            y1 = display_info.y;
-        } else if y1 > screen_y2 {
-            y1 = screen_y2;
-        }
-
-        if x2 > screen_x2 {
-            x2 = screen_x2;
-        }
-
-        if y2 > screen_y2 {
-            y2 = screen_y2;
-        }
-
-        if x1 >= x2 || y1 >= y2 {
-            return Err(anyhow!("Area size is invalid"));
-        }
-
-        capture_screen_area(
-            &display_info,
-            x1 - display_info.x,
-            y1 - display_info.y,
-            (x2 - x1) as u32,
-            (y2 - y1) as u32,
-        )
     }
 }
